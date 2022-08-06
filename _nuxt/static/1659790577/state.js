331d0d75457af1ee5f341f9c7fdc20fec1605f4e
abner <abner.chamorro@inetum.com>
2022-08-06 07:56:28 -0500

    4f7e4a6

---
+++ _nuxt/static/1659790577/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002Fapp-detail\u002F_nuxt\u002Fstatic\u002F1659790577",layout:"default",error:a,state:{product:{product:{code:8232,name:""},brand:{}}},serverRendered:true,routePath:"\u002F",config:{_app:{basePath:"\u002Fapp-detail\u002F",assetsPath:"\u002Fapp-detail\u002F_nuxt\u002F",cdnURL:a}}}}(null));
